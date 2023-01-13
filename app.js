f0e8678176dff434f442614c6cf4de5b816c34d5
Krish1116 <chotaliyakrishana1116@gmail.com>
2023-01-13 10:03:38 +0530

    getelementbyclassname

--- app.js
+++ app.js
@@ -30,14 +30,38 @@
 // tag.style.color = 'green'
 
 //getelementbyclassname
-let items = document.getElementsByClassName('list-group-item');
-console.log(typeof(items));
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[2].style.backgroundColor = 'green';
+// let items = document.getElementsByClassName('list-group-item');
+// console.log(typeof(items));
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[2].style.backgroundColor = 'green';
 
 
-for(let i = 0; i < items.length; i++){
-    items[i].style.fontWeight = 'bold';
-}
+// for(let i = 0; i < items.length; i++){
+//     items[i].style.fontWeight = 'bold';
+// }
+
+//getelementbyTagName
+// let li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[2].style.backgroundColor = 'green';
+
+
+// for(let i = 0; i < li.length; i++){
+//     li.style.fontWeight = 'bold';
+// }
+
+//queryselector 
+let header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+let input = document.querySelector('input');
+input.value = 'Hello World';
+
+let submit = document.querySelector('input[type="submit"]');
+submit.value = "SEND"
+
