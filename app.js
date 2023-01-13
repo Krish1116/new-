fcd01d8ab3da392c8f05f88960010bdc95be51fd
Krish1116 <chotaliyakrishana1116@gmail.com>
2023-01-13 09:43:42 +0530

    getelementbyClassName

--- app.js
+++ app.js
@@ -15,16 +15,29 @@
 
 //getelementbyid
 // console.log(document.getElementById('header-title'))
-var headerTitle = document.getElementById('header-title');
-var headerTitle = document.getElementById('main-header');
+// var headerTitle = document.getElementById('header-title');
+// var headerTitle = document.getElementById('main-header');
 // console.log(typeof(headerTitle));
 // headerTitle.textContent = 'Hello';
 // headerTitle.innerText = 'Goodbye';
 // console.log(headerTitle.innerText);
 // headerTitle.innerHTML = '<h3>Hello</h3>'
-headerTitle.style.borderBottom = 'solid 3px #000'
+// headerTitle.style.borderBottom = 'solid 3px #000'
 
 // console.log(document.getElementsByClassName('title').style.color = 'green');
 // console.log(x);
-const tag = document.querySelector('.title');
-tag.style.color = 'green'
+// const tag = document.querySelector('.title');
+// tag.style.color = 'green'
+
+//getelementbyclassname
+let items = document.getElementsByClassName('list-group-item');
+console.log(typeof(items));
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[2].style.backgroundColor = 'green';
+
+
+for(let i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
