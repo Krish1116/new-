7f6119a31d208eaebc25061fe16651c14adf8d48
Krish1116 <chotaliyakrishana1116@gmail.com>
2023-01-16 14:17:30 +0530

    store obj in local storage

--- Booking appointment App/app.js
+++ Booking appointment App/app.js
@@ -1,10 +1,28 @@
+// let submit = document.getElementById('my-form');
+// submit.addEventListener('submit', addData);
+
+// function addData(e){
+//     e.preventDefault();
+//     const name = e.target.name.value;
+//     const email = e.target.email.value;
+//     const ph_no = e.target.ph_no.value;
+//     const date = e.target.date.value;
+    // localStorage.setItem('name', name)
+    localStorage.setItem('email', email)
+//     localStorage.setItem('ph_no', ph_no)
+//     localStorage.setItem('date', date)
+// }
+
 let submit = document.getElementById('my-form');
 submit.addEventListener('submit', addData);
 
-function addData(){
+function addData(e){
+    window.localStorage.clear();
+    e.preventDefault();
+
     let name = document.getElementById('name').value;
     localStorage.useName = name;
-    
+         
     let email = document.getElementById('email').value;
     localStorage.userEmail = email;
     
@@ -13,4 +31,25 @@ function addData(){
 
     let date = document.getElementById('date').value;
     localStorage.userDate = date;
+    // console.log(localStorage);
+
+    let obj = {
+        name,
+        email,
+        ph_no,
+        date
+    }
+
+    let obj_serialized = JSON.stringify(obj); //converting it into some form of readable data
+    console.log(obj_serialized);
+
+    localStorage.setItem('myObj', obj_serialized);
+    // console.log(localStorage);
+
+    // let obj_deserialized = JSON.parse(localStorage.getItem(obj));
+    // console.log(obj_deserialized);
 }
+
+
+
+
